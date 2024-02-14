d11f5d861bacca8646e3e918208d666a74e6b293
Nicolas Cartagena <nicolas.a.cartagena.m@gmail.com>
2024-02-14 17:15:35 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <div className="container-fluid">
-      <h1 className="text-center display-1 py-4">Rick and Morty</h1>
+      <h1 className="text-center display-1 py-4">Rick and Morty Saludos Gonza</h1>
       <div className="container">
         {loading ? 'Cargando...' : <Content characters={characters} />}
         {loading ? '' : <Pagination numPages={numPages} selectPage={selectPage} previousPage={previousPage} nextPage={nextPage} actualPage={page} />}
